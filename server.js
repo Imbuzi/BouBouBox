ae1f9061060e5d9b35728c4c05df6f9d0e8706bb
Nicolas Bourasseau <nicolas.bourasseau@outlook.com>
2017-09-11 14:58:44 +0200

    Enabled HTTPS with Let's Encrypt Certificates + Added .gitignore file

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
-var http = require('http');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
+const helmet = require('helmet');
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello world !');
+// Déclaration application Express + Middlewares
+const app = express();
+app.use(helmet());
+
+// Redirection HTTP 301 en HTTPS
+http.createServer(function(req, res) {   
+        res.writeHead(301, {"Location": "https://" + req.headers['host'] + req.url});
+        res.end();
+}).listen(3000);
+
+// Routage Express
+app.get('/hello-world',function(req, res) {
+	res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World !');
 });
 
-server.listen(3000);
+// Création du serveur HTTPS (certificats compris)
+https.createServer({ 
+        key: fs.readFileSync("/etc/letsencrypt/live/box.boubou.io/privkey.pem"),
+        cert: fs.readFileSync("/etc/letsencrypt/live/box.boubou.io/fullchain.pem"),
+        ca: fs.readFileSync("/etc/letsencrypt/live/box.boubou.io/chain.pem")
+}, app).listen(4000);
