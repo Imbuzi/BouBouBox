a8ee230a802935196c2e180b5b1b6516a7730dd7
Nicolas BOURASSEAU <nicolas.bourasseau@outlook.com>
2017-09-19 21:49:22 +0200

    Removed some unused buttons in navbar

--- components/navbar-collapse.vue
+++ components/navbar-collapse.vue
@@ -1,21 +1,6 @@
 <template>
 	<div class="collapse navbar-collapse" id="navbar-collapse">
 		<ul class="nav navbar-nav navbar-right">
-			<li class="dropdown">
-				<a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button">
-					<i class="material-icons">notifications</i>
-					<span v-if="count" class="label-count">{{count}}</span>
-				</a>
-        <ul class="dropdown-menu">
-          <li>
-            <a href="#">Action</a>
-          </li>
-          <li role="separator" class="divider"></li>
-          <li>
-            <a href="#">Separated link</a>
-          </li>
-        </ul>
-			</li>
 			<li class="pull-right"><a href="#" v-on:click.prevent="" class="js-right-sidebar" data-close="true"><i class="material-icons">menu</i></a></li>
 		</ul>
 	</div>
