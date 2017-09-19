61bca2016f68693b0126ce6b93ae07ff09679669
Nicolas BOURASSEAU <nicolas.bourasseau@outlook.com>
2017-09-19 22:15:43 +0200

    Changed element classes

--- components/navbar-header.vue
+++ components/navbar-header.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="navbar-header">
-	  <div class="col-xs-10">
+  <div class="row">
+	  <div class="navbar-header col-xs-10">
       <a class="navbar-brand" v-on:click.prevent="" href="#">{{title}}</a>
 	  </div>
-    <div class="col-xs-2">
+    <div class="navbar-header col-xs-2">
       <div class="pull-right">
         <a class="navbar-brand" v-on:click.prevent="" href="#">
           <i class="material-icons">menu</i>
