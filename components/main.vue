3eae1696b71a32d7e434f9085e2bded56a131bdc
Nicolas BOURASSEAU <nicolas.bourasseau@outlook.com>
2017-09-19 22:39:52 +0200

    Added first widget panel implementation

--- components/main.vue
+++ components/main.vue
@@ -1,20 +1,23 @@
 <template>
   <body v-bind:class="'theme-' + color">
     <navbar></navbar>
+    <widget-panel></widget-panel>
   </body>
 </template>
 
 <script>
-	var NavBar = require('./navbar.vue');
+  var NavBar = require('./navbar.vue');
+  var WidgetPanel = require('./widget-panel.vue');
 
-	module.exports = {
+  module.exports = {
     data: function() {
       return {
-        'color': 'blue'
+      'color': 'blue'
       }
     },
-		components: {
-			'navbar': NavBar
-		}
-	}
+    components: {
+      'navbar': NavBar,
+      'widget-panel': WidgetPanel
+    }
+  }
 </script>
---
+++ components/widget-panel.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="card"></div>"
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+	module.exports = {}
+</script>
