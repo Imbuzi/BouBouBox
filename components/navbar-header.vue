12bc3a964e0411c22a3edcb4516dfac0ebcdc044
Nicolas BOURASSEAU <nicolas.bourasseau@outlook.com>
2017-09-19 22:14:16 +0200

    Corrected template root number of elements

--- components/navbar-header.vue
+++ components/navbar-header.vue
@@ -1,12 +1,14 @@
 <template>
-	<div class="navbar-header col-xs-10">
-    <a class="navbar-brand" v-on:click.prevent="" href="#">{{title}}</a>
-	</div>
-  <div class="navbar-header col-xs-2">
-    <div class="pull-right">
-      <a class="navbar-brand" v-on:click.prevent="" href="#">
-        <i class="material-icons">menu</i>
-      </a>
+  <div class="navbar-header">
+	  <div class="col-xs-10">
+      <a class="navbar-brand" v-on:click.prevent="" href="#">{{title}}</a>
+	  </div>
+    <div class="col-xs-2">
+      <div class="pull-right">
+        <a class="navbar-brand" v-on:click.prevent="" href="#">
+          <i class="material-icons">menu</i>
+        </a>
+      </div>
     </div>
   </div>
 </template>
