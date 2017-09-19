9eddc9650513f356810e0a38a37eaa168b3651d2
Nicolas BOURASSEAU <nicolas.bourasseau@outlook.com>
2017-09-19 21:53:12 +0200

    Removed unused responsive navbar feature

--- components/navbar-header.vue
+++ components/navbar-header.vue
@@ -1,6 +1,5 @@
 <template>
 	<div class="navbar-header">
-		<a class="navbar-toggle collapsed" v-on:click.prevent="" href="#" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
 		<a class="bars" v-on:click.prevent="" href="#" ></a>
 		<a class="navbar-brand" v-on:click.prevent="" href="#">{{title}}</a>
 	</div>
