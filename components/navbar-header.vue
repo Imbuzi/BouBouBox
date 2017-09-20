3db9fbe0efd1d8377d7a6fdb89c64d707c82ebab
Imbuzi <nicolas.bourasseau@outlook.com>
2017-09-20 16:08:23 +0200

    Added transition on menu icon

--- components/navbar-header.vue
+++ components/navbar-header.vue
@@ -6,7 +6,10 @@
     <div class="navbar-header col-xs-2">
       <div class="pull-right">
         <a class="navbar-brand" v-on:click.prevent.left="toggleMenu" href="#">
-          <i class="material-icons">menu</i>
+          <transition name="fade" mode="out-in">
+            <i v-if="opened" class="material-icons">menu</i>
+            <i v-if="!opened" class="material-icons">close</i>
+          </transition>
         </a>
       </div>
     </div>
@@ -16,9 +19,15 @@
 <script>
 	module.exports = {
 		props: ['title'],
+    data: function() {
+      return {
+        'opened': false
+      }
+    },
     methods: {
       toggleMenu: function(event) {
         // Received 'click' event, let's send it to the parent component !
+        this.opened = !this.opened;
         this.$emit('toggle-menu');
       }
     }
