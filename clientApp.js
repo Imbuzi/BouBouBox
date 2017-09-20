a4ce5544f8572045201d4b036e75529c8704410b
Nicolas Bourasseau <nicolas.bourasseau@etu.enseeiht.fr>
2017-09-20 11:32:56 +0200

    Improved App debug testing

--- clientApp.js
+++ clientApp.js
@@ -1,8 +1,5 @@
-// Testing
-var ApplicationVue;
-
 document.addEventListener("DOMContentLoaded", function (event) {
 	require('./client_modules/io.js');
-    ApplicationVue = require('./client_modules/vue.js');
+    require('./client_modules/vue.js');
 	require('./scripts/script.js');
 });
