7b706554bd7abb2cef967cf4e4256e9c9e7b8eab
Imbuzi <nicolas.bourasseau@outlook.com>
2017-09-21 09:18:08 +0200

    Edited socket.io server host address

--- client_modules/vue.js
+++ client_modules/vue.js
@@ -3,7 +3,7 @@ const SocketIOClient = require('socket.io-client');
 const VueSocketIO = require('vue-socket.io');
 const MainVueComponent = require('../components/main.vue')
 
-Vue.use(VueSocketIO, SocketIOClient('http://localhost'));
+Vue.use(VueSocketIO, SocketIOClient('http://box.boubou.io'));
 
 const App = new Vue({
     el: 'body',
