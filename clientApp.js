d58099d2615f3742549083c89a9690bb55ed346d
Imbuzi <nicolas.bourasseau@outlook.com>
2017-09-25 17:48:37 +0200

    Corrected bad merge

--- clientApp.js
+++ clientApp.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Vue from 'vue';
 import Vuex from 'vuex';
 import SocketIO from 'socket.io-client';
@@ -6,15 +5,6 @@ import VueSocketIO from 'vue-socket.io';
 import MainVueComponent from './components/main.vue';
 
 document.addEventListener("DOMContentLoaded", function (event) {
-=======
-document.addEventListener("DOMContentLoaded", function (event) {
-    import Vue from 'vue';
-	import Vuex from 'vuex';
-	import SocketIO from 'socket.io-client';
-	import VueSocketIO from 'vue-socket.io';
-	import MainVueComponent from 'components/main.vue';
-
->>>>>>> fcd4ade5097048123111d7facc61ef11d1cbdbc9
 	const SocketIOInstance = SocketIO('http://box.boubou.io')
 
 	Vue.use(Vuex);
