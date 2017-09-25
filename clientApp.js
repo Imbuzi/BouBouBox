fe84c34dc187d8ea6ba600e6f00f885f4c43aaee
Nicolas Bourasseau <nicolas.bourasseau@outlook.com>
2017-09-25 17:15:23 +0200

    Corrected last commit

--- clientApp.js
+++ clientApp.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    import Vue from 'vue';
-	import Vuex from 'vuex';
-	import SocketIO from 'socket.io-client';
-	import VueSocketIO from 'vue-socket.io';
-	import MainVueComponent from 'components/main.vue';
+import Vue from 'vue';
+import Vuex from 'vuex';
+import SocketIO from 'socket.io-client';
+import VueSocketIO from 'vue-socket.io';
+import MainVueComponent from './components/main.vue';
 
+document.addEventListener("DOMContentLoaded", function (event) {
 	const SocketIOInstance = SocketIO('http://box.boubou.io')
 
 	Vue.use(Vuex);
