7a786675e8a4d0ea50c0f364f57c92f8106bc2c7
Imbuzi <nicolas.bourasseau@outlook.com>
2017-09-26 10:19:32 +0200

    Changed store syntax

--- clientApp.js
+++ clientApp.js
@@ -2,19 +2,20 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import SocketIO from 'socket.io-client';
 import VueSocketIO from 'vue-socket.io';
-import Store from './store';
 import MainVueComponent from './components/main.vue';
 
-console.log(Store);
+import store from './store';
+
+console.log(store);
 
 document.addEventListener("DOMContentLoaded", function (event) {
 	const SocketIOInstance = SocketIO('http://box.boubou.io')
 
-	Vue.use(VueSocketIO, SocketIOInstance, Store);
+	Vue.use(VueSocketIO, SocketIOInstance, store);
 
 	const App = new Vue({
 		el: 'main',
-		Store,
+		store,
 		sockets: {},
 		render: function (createElement) {
 			return createElement(MainVueComponent);
