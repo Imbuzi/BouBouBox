f23bbce3341e52625a24fcd46f03337c975fa41f
Imbuzi <nicolas.bourasseau@outlook.com>
2017-09-28 16:06:37 +0200

    Corrected CSS

--- components/widget.vue
+++ components/widget.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-        <div class="main-widget-part">
+    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 main-widget-container">
+        <div>
             WIDGET
         </div>
     </div>
@@ -11,11 +11,11 @@
 </script>
 
 <style scoped>
-    .main-widget-part:not(:nth-of-type(4n)):not(:last-of-type) {
+    .main-widget-container:not(:nth-child(4n+0)):not(:last-child) {
         border-right: 1px solid #9E9E9E;
     }
 
-    .main-widget-part {
+    .main-widget-container {
         margin-top: 20px;
         margin-bottom: 20px;
     }
