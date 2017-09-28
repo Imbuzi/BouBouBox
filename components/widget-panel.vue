9773092bd561ae3f7f93cb12a2b5482eba6b718a
Imbuzi <nicolas.bourasseau@outlook.com>
2017-09-28 15:11:17 +0200

    Testing light panels size

--- components/widget-panel.vue
+++ components/widget-panel.vue
@@ -1,16 +1,21 @@
 <template>
-  <article>
-    <div class="container-fluid">
-      <div class="block-header">
-        <h2>{{panelTitle}}</h2>
-      </div>
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="card"></div>
+    <article>
+        <div class="container-fluid">
+            <div class="block-header">
+                <h2>{{panelTitle}}</h2>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="card">
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </article>
+    </article>
 </template>
 
 <script>
