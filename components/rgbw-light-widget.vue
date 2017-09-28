d886b2325d64a9e54e4db488549f97f50ec172d0
Imbuzi <nicolas.bourasseau@outlook.com>
2017-09-28 19:13:25 +0200

    Added more button

--- components/rgbw-light-widget.vue
+++ components/rgbw-light-widget.vue
@@ -2,7 +2,7 @@
     <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
         <div class="card">
             <div class="body">
-                <div class="m-b--35 font-bold">{{roomName}}</div>
+                <div class="font-bold uppercase">{{roomName}}<div class="pull-right widget-button"><a href="#"><i class="material-icons">more_vert</i></a></div></div>
                 <div class="align-center">
                     <div class="color-wheel-container">
                         <color-wheel></color-wheel>
@@ -30,7 +30,13 @@
         margin: auto;
     }
 
-    .widget-title {
-        font-size: 16px;
+    .widget-button a i {
+        font-size: 20px;
+        margin-top: -2px;
+        color: #555;
+    }
+
+    .uppercase {
+        text-transform: uppercase;
     }
 </style>
