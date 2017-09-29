86dc0eadb296d7b3bb41f1c3ac17517a495cbd86
Imbuzi <nicolas.bourasseau@outlook.com>
2017-09-29 14:50:40 +0200

    Added all colors for connect bars

--- components/intensity-slider.vue
+++ components/intensity-slider.vue
@@ -38,94 +38,89 @@
         top: -9px !important;
         outline: none;
         border: none !important;
-        background: #3FB8AF !important;
     }
 
     .noUi-handle:not(.noUi-active) {
         box-shadow: none !important;
     }
 
-    .theme-red .noUi-handle {
+    .theme-red .noUi-handle, .theme-red .noUi-connect {
         background-color: #F44336;
     }
 
-    .theme-pink .noUi-handle {
+    .theme-pink .noUi-handle, .theme-pink .noUi-connect {
         background-color: #E91E63;
     }
 
-    .theme-purple .noUi-handle {
+    .theme-purple .noUi-handle, .theme-purple .noUi-connect {
         background-color: #9C27B0;
     }
 
-    .theme-deep-purple .noUi-handle {
+    .theme-deep-purple .noUi-handle, .theme-deep-purple .noUi-connect {
         background-color: #673AB7;
     }
 
-    .theme-indigo .noUi-handle {
+    .theme-indigo .noUi-handle, .theme-indigo .noUi-connect {
         background-color: #3F51B5;
     }
 
-    .theme-blue .noUi-handle {
+    .theme-blue .noUi-handle, .theme-blue .noUi-connect {
         background-color: #2196F3;
     }
 
-    .theme-light-blue .noUi-handle {
+    .theme-light-blue .noUi-handle, .theme-light-blue .noUi-connect {
         background-color: #03A9F4;
     }
 
-    .theme-cyan .noUi-handle {
+    .theme-cyan .noUi-handle, .theme-cyan .noUi-connect {
         background-color: #00BCD4;
     }
 
-    .theme-teal .noUi-handle {
+    .theme-teal .noUi-handle, .theme-teal .noUi-connect {
         background-color: #009688;
     }
 
-    .theme-green .noUi-handle {
+    .theme-green .noUi-handle, .theme-green .noUi-connect {
         background-color: #4CAF50;
     }
 
-    .theme-light-green .noUi-handle {
+    .theme-light-green .noUi-handle, .theme-light-green .noUi-connect {
         background-color: #8BC34A;
     }
 
-    .theme-lime .noUi-handle {
+    .theme-lime .noUi-handle, .theme-lime .noUi-connect {
         background-color: #CDDC39;
     }
 
-    .theme-yellow .noUi-handle {
+    .theme-yellow .noUi-handle, .theme-yellow .noUi-connect {
         background-color: #FFEB3B;
     }
 
-    .theme-amber .noUi-handle {
+    .theme-amber .noUi-handle, .theme-amber .noUi-connect {
         background-color: #FFC107;
     }
 
-    .theme-orange .noUi-handle {
+    .theme-orange .noUi-handle, .theme-orange .noUi-connect {
         background-color: #FF9800;
     }
 
-    .theme-orange .noUi-handle {
-        background-color: #FF9800;
-    }
-
-    .theme-deep-orange .noUi-handle {
+    .theme-deep-orange .noUi-handle, .theme-deep-orange .noUi-connect {
         background-color: #FF5722;
     }
 
-    .theme-brown .noUi-handle {
+    .theme-brown .noUi-handle, .theme-brown .noUi-connect {
         background-color: #795548;
     }
 
-    .theme-grey .noUi-handle {
+    .theme-grey .noUi-handle, .theme-grey .noUi-connect {
         background-color: #9E9E9E;
     }
 
-    .theme-blue-grey .noUi-handle {
+    .theme-blue-grey .noUi-handle, .theme-blue-grey .noUi-connect {
         background-color: #607D8B;
     }
 
-    .theme-black .noUi-handle {
+    .theme-black .noUi-handle, .theme-black .noUi-connect {
         background-color: #000;
     }
 </style>
