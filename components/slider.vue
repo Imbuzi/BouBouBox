b4ce02cdc9733194b4002d1d841fe7c8a86e069e
Imbuzi <nicolas.bourasseau@outlook.com>
2017-09-29 08:35:23 +0200

    Added first noUiSlider test

--- components/slider.vue
+++ components/slider.vue
@@ -1,10 +1,24 @@
 <template>
-    <div>
+    <div class="slider">
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        mounted: function() {
+            var noUiSlider = require('nouislider');
+            var slider = document.querySelector('.slider');
+
+            noUiSlider.create(slider, {
+                start: 40,
+                connect: "lower",
+                range: {
+                    min: 0,
+                    max: 100
+                }
+            });
+        }
+    }
 </script>
 
 <style scoped>
