75cdac0bf23726ab7a6f522d883c1d8954f8c1d1
Imbuzi <nicolas.bourasseau@outlook.com>
2017-09-30 10:23:13 +0200

    Corrected query SQL

--- model/db.js
+++ model/db.js
@@ -14,9 +14,9 @@ db.panel.getAll = () => knex.select().from('panel');
 
 db.room.getAll = function () {
     knex
-        .select('room.id as id', 'room.name as name', 'room.zone as zone', 'room.type as type', 'bridge.mac as router_mac', 'bridge.name as router_name')
         .from('room')
         .innerJoin('bridge', 'room.router', 'bridge.id')
+        .select('room.id as id', 'room.name as name', 'room.zone as zone', 'room.type as type', 'bridge.mac as router_mac', 'bridge.name as router_name')
         .then(function (result) {
             /*result.router = {
                 name: result.router_name,
