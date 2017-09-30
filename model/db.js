0b6275a7ae0e98113ea2128cf1fea3e040744637
Imbuzi <nicolas.bourasseau@outlook.com>
2017-09-30 10:06:16 +0200

    Changed query structure

--- model/db.js
+++ model/db.js
@@ -12,7 +12,22 @@ const db = {
 
 db.panel.getAll = () => knex.select().from('panel');
 
-db.room.getAll = () => knex.select('room.id as id','room.name as name','room.zone as zone','room.type as type','bridge.mac as router_mac','bridge.name as router_name').from('room').innerJoin('bridge', 'room.router', 'bridge.id');
+db.room.getAll = function () {
+    knex
+        .select('room.id as id', 'room.name as name', 'room.zone as zone', 'room.type as type', 'bridge.mac as router_mac', 'bridge.name as router_name')
+        .from('room')
+        .innerJoin('bridge', 'room.router', 'bridge.id')
+        .then(function (result) {
+            result.router = {
+                name: result.router_name,
+                mac: result.router_mac
+            };
+            delete result.router_name;
+            delete result.router_mac;
+
+            return result;
+        });
+}
 
 db.bridge.getAll = () => knex.select().from('bridge');
 
