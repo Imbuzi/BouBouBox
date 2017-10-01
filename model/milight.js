e69f85b469326c8e088f009c0647b297f99eb017
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-01 11:17:37 +0200

    Corrected Promise function structure

--- model/milight.js
+++ model/milight.js
@@ -2,22 +2,24 @@ const milight = require('node-milight-promise');
 
 let exp = {};
 
-exp.createBridges = milight.discoverBridges({
+exp.createBridges = function () {
+    return milight.discoverBridges({
         type: 'all'
-}).then(function (results) {
-    let bridges = [];
-    results.forEach(function (element) {
-        let bridge = new milight.MilightController({
-            ip: element.ip,
-            type: element.type
+    }).then(function (results) {
+        let bridges = [];
+        results.forEach(function (element) {
+            let bridge = new milight.MilightController({
+                ip: element.ip,
+                type: element.type
+            });
+            bridge.mac = element.mac;
+            bridges.push(bridge);
         });
-        bridge.mac = element.mac;
-        bridges.push(bridge);
+
+        console.log(bridges);
+
+        return bridges;
     });
-
-    console.log(bridges);
-
-    return bridges;
-});
+}
 
 module.exports = exp;
