be5970626e49668f0b8cbeb196b7e0c2a6b3a71f
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-01 11:11:42 +0200

    Corrected createBridges function

--- model/milight.js
+++ model/milight.js
@@ -2,22 +2,20 @@ const milight = require('node-milight-promise');
 
 let exp = {};
 
-exp.createBridges = function () {
-    let bridges = [];
-    milight.discoverBridges({
+exp.createBridges = milight.discoverBridges({
         type: 'all'
-    }).then(function (results) {
-        results.forEach(function (element) {
-            let bridge = new milight.MilightController({
-                ip: element.ip,
-                type: element.type
-            });
-            bridge.mac = element.mac;
-            bridges.push(bridge);
+}).then(function (results) {
+    let bridges = [];
+    results.forEach(function (element) {
+        let bridge = new milight.MilightController({
+            ip: element.ip,
+            type: element.type
         });
-
-        return bridges;
+        bridge.mac = element.mac;
+        bridges.push(bridge);
     });
-}
+
+    return bridges;
+});
 
 module.exports = exp;
