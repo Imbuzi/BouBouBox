f752626ab0f24ac1c545a59c01f89fe723f31c19
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-03 08:57:04 +0200

    Removed unused queries

--- model/db.js
+++ model/db.js
@@ -5,32 +5,8 @@ const knex = require('knex')(config);
 console.log(config);
 
 const db = {
-    panel: {},
-    room: {},
-    bridge: {},
     widget: {}
 };
-
-db.panel.getAll = () => knex.select().from('panel');
-
-db.room.getAll = function () {
-    return knex
-        .from('room')
-        .innerJoin('bridge', 'room.router', 'bridge.id')
-        .select('room.id as id', 'room.name as name', 'room.zone as zone', 'room.type as type', 'bridge.mac as router_mac', 'bridge.name as router_name')
-        .then(function (result) {
-            result.forEach(function (element) {
-                element.router = {
-                    name: element.router_name,
-                    mac: element.router_mac
-                };
-                delete element.router_name;
-                delete element.router_mac;
-            });
-
-            return result;
-        });
-}
 
 db.widget.getAll = function () {
     return knex
