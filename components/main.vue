b17c2245bf0c6d81f672d50fe67dbb6c1d1939cd
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-03 09:39:03 +0200

    Improved main.vue structure

--- components/main.vue
+++ components/main.vue
@@ -1,28 +1,28 @@
 <template>
-  <main v-bind:class="'theme-' + color">
-    <navbar></navbar>
-    <sidebar></sidebar>
-    <main-section></main-section>
-  </main>
+    <main v-bind:class="'theme-' + color">
+        <navbar></navbar>
+        <sidebar></sidebar>
+        <main-section></main-section>
+    </main>
 </template>
 
 <script>
-  import NavBar from './navbar.vue';
-  import MainSection from './main-section.vue';
-  import SideBar from './sidebar.vue';
+    import NavBar from './navbar.vue';
+    import MainSection from './main-section.vue';
+    import SideBar from './sidebar.vue';
 
-  export default {
-    computed: {
-      color: function() {
-        return this.$store.state.theme.color;
-      }
-    },
-    components: {
-      'navbar': NavBar,
-      'main-section': MainSection,
-      'sidebar': SideBar
+    export default {
+        computed: {
+            color: function() {
+                return this.$store.state.theme.color;
+            }
+        },
+        components: {
+            'navbar': NavBar,
+            'main-section': MainSection,
+            'sidebar': SideBar
+        }
     }
-  }
 </script>
 
 <style scoped>
