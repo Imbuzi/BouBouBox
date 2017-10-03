8602f15c36828df093bcf4dac6dffe2c97cd69f8
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-03 11:56:57 +0200

    Edited login vue

--- components/login.vue
+++ components/login.vue
@@ -1,48 +1,57 @@
 <template>
-    <div class="card">
-        <div class="body">
-            <form>
-                <div class="msg">Sign in to start your session</div>
-                <div class="input-group">
-                    <span class="input-group-addon">
-                        <i class="material-icons">person</i>
-                    </span>
-                    <div class="form-line">
-                        <input type="text" class="form-control" name="username" placeholder="Username" required autofocus>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-10 col-sm-8 col-md-6 col-lg-4">
+                <div class="card">
+                    <div class="body">
+                        <form>
+                            <h4>{{title}}</h4>
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">person</i>
+                                </span>
+                                <div class="form-line">
+                                    <input type="text" class="form-control" name="mail" placeholder="Adresse mail" required autofocus>
+                                </div>
+                            </div>
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">lock</i>
+                                </span>
+                                <div class="form-line">
+                                    <input type="password" class="form-control" name="password" placeholder="Mot de passe" required>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-8 p-t-5">
+                                    <input type="checkbox" name="rememberme" class="filled-in chk-col-pink">
+                                    <label for="rememberme">Rester connecté</label>
+                                </div>
+                                <div class="col-xs-4">
+                                    <button class="btn btn-block bg-pink waves-effect" type="submit">SE CONNECTER</button>
+                                </div>
+                            </div>
+                            <div class="row m-t-15 m-b--20">
+                                <div class="col-xs-6">
+                                    <a href="#">Créer un compte</a>
+                                </div>
+                                <div class="col-xs-6 align-right">
+                                    <a href="#">Mot de passe oublié</a>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
-                <div class="input-group">
-                    <span class="input-group-addon">
-                        <i class="material-icons">lock</i>
-                    </span>
-                    <div class="form-line">
-                        <input type="password" class="form-control" name="password" placeholder="Password" required>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-xs-8 p-t-5">
-                        <input type="checkbox" name="rememberme" id="rememberme" class="filled-in chk-col-pink">
-                        <label for="rememberme">Remember Me</label>
-                    </div>
-                    <div class="col-xs-4">
-                        <button class="btn btn-block bg-pink waves-effect" type="submit">SIGN IN</button>
-                    </div>
-                </div>
-                <div class="row m-t-15 m-b--20">
-                    <div class="col-xs-6">
-                        <a href="sign-up.html">Register Now!</a>
-                    </div>
-                    <div class="col-xs-6 align-right">
-                        <a href="forgot-password.html">Forgot Password?</a>
-                    </div>
-                </div>
-            </form>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     export default {
+        'title': function () {
+            return this.$store.state.sidebar.title;
+        }
     }
 </script>
 
