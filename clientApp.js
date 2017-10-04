151e3643b595f104fee34f60beee92d2241f474b
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-04 08:43:32 +0200

    Added vue router code

--- clientApp.js
+++ clientApp.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VueRouter from 'vue-router';
 import SocketIO from 'socket.io-client';
 import VueSocketIO from 'vue-socket.io';
 import MainVueComponent from './components/main.vue';
 
 import store from './store';
+import router from './router';
 
 document.addEventListener("DOMContentLoaded", function (event) {
 	const SocketIOInstance = SocketIO('http://box.boubou.io')
@@ -13,7 +15,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 	const App = new Vue({
 		el: 'main',
-		store,
+        store,
+        router,
 		sockets: {},
 		render: function (createElement) {
 			return createElement(MainVueComponent);
