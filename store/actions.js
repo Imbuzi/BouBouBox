f90cee4c5d192b06302973859d73f60dedfa1df2
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-05 11:38:15 +0200

    Corrected showAlert action

--- store/actions.js
+++ store/actions.js
@@ -3,7 +3,7 @@ export const showAlert = function(context, payload) {
     context.commit('toggleAlert', true);
     clearTimeout(context.state.alert.timeout);
     let timeout = setTimeout(function () {
-        commit('toggleAlert', false);
+        context.commit('toggleAlert', false);
     }, payload.delay);
     context.commit('setTimeout', timeout);
 }
