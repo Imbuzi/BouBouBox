f3067792970f27753f01e178b0609368f9d12de5
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-05 08:52:01 +0200

    Corrected login color

--- components/login.vue
+++ components/login.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+            <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
                 <div class="card">
                     <div class="body">
                         <form v-on:submit.prevent="submitForm">
@@ -107,6 +107,11 @@
                 } else {
                     console.log(result);
                 }
+            }
+        },
+        computed: {
+            color: function () {
+                return this.$store.state.theme.color;
             }
         }
     }
