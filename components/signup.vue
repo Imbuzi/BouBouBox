268c4dcb135c6ac5a355a8266e3fd7923b8c5065
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-10 16:15:10 +0200

    Added final signup component

--- components/signup.vue
+++ components/signup.vue
@@ -16,13 +16,13 @@
                                 </span>
                                 <div class="row">
                                     <div class="col-xs-6">
-                                        <div class="form-line">
-                                            <input type="text" class="form-control" name="name" placeholder="Prénom" required autofocus>
+                                        <div class="form-line" v-bind:class="{ focused: nameFocused }">
+                                            <input v-bind:disabled="formLocked" type="text" class="form-control" v-model="name" v-on:blur="toggleNameFocused(false)" v-on:focus="toggleNameFocused(true)" name="name" placeholder="Prénom" required autofocus>
                                         </div>
                                     </div>
                                     <div class="col-xs-6">
-                                        <div class="form-line">
-                                            <input type="text" class="form-control" name="surname" placeholder="Nom" required>
+                                        <div class="form-line" v-bind:class="{ focused: surnameFocused }">
+                                            <input v-bind:disabled="formLocked" type="text" class="form-control" v-model="surname" v-on:blur="toggleSurnameFocused(false)" v-on:focus="toggleSurnameFocused(true)" name="surname" placeholder="Nom" required>
                                         </div>
                                     </div>
                                 </div>
@@ -31,24 +31,24 @@
                                 <span class="input-group-addon">
                                     <i class="material-icons">email</i>
                                 </span>
-                                <div class="form-line">
-                                    <input type="email" class="form-control" name="email" placeholder="Adresse mail" required>
+                                <div class="form-line" v-bind:class="{ focused: mailFocused }">
+                                    <input v-bind:disabled="formLocked" type="email" class="form-control" v-model="mailAddress" v-on:blur="toggleMailFocused(false)" v-on:focus="toggleMailFocused(true)" name="email" placeholder="Adresse mail" required>
                                 </div>
                             </div>
                             <div class="input-group">
                                 <span class="input-group-addon">
                                     <i class="material-icons">lock</i>
                                 </span>
-                                <div class="form-line">
-                                    <input type="password" class="form-control" name="password" placeholder="Mot de passe" required>
+                                <div class="form-line" v-bind:class="{ focused: passwordFocused }">
+                                    <input v-bind:disabled="formLocked" type="password" class="form-control" v-model="password" v-on:blur="togglePasswordFocused(false)" v-on:focus="togglePasswordFocused(true)" name="password" placeholder="Mot de passe" required>
                                 </div>
                             </div>
                             <div class="input-group">
                                 <span class="input-group-addon">
                                     <i class="material-icons">lock</i>
                                 </span>
-                                <div class="form-line">
-                                    <input type="password" class="form-control" name="confirm" placeholder="Confirmation du mot de passe" required>
+                                <div class="form-line" v-bind:class="{ focused: passwordConfirmFocused }">
+                                    <input v-bind:disabled="formLocked" type="password" class="form-control" v-model="passwordConfirm" v-on:blur="togglePasswordConfirmFocused(false)" v-on:focus="togglePasswordConfirmFocused(true)" name="confirm" placeholder="Confirmation du mot de passe" required>
                                 </div>
                             </div>
                             <button v-bind:class="'bg-' + color" class="btn btn-block btn-lg" type="submit">CRÉER LE COMPTE</button>
@@ -65,9 +65,45 @@
 
 <script>
     export default {
+        data: function () {
+            return {
+                name: '',
+                surname: '',
+                mailAddress: '',
+                password: '',
+                passwordConfirm: '',
+                nameFocused: false,
+                surnameFocused: false,
+                mailAddressFocused: false,
+                passwordFocused: false,
+                passwordConfirmFocused: false,
+                loading: false,
+                formLocked: false
+            }
+        },
         methods: {
+            toggleMailFocused: function (value) {
+                this.mailFocused = value;
+            },
+            toggleNameFocused: function (value) {
+                this.nameFocused = value;
+            },
+            toggleSurnameFocused: function (value) {
+                this.surnameFocused = value;
+            },
+            togglePasswordFocused: function (value) {
+                this.passwordFocused = value;
+            },
+            togglePasswordConfirmFocused: function (value) {
+                this.passwordConfirmFocused = value;
+            },
+            lockForm: function (value) {
+                this.formLocked = value;
+            },
             submitForm: function () {
-
+                this.toggleLoading(true);
+                this.lockForm(true);
+                // DO STUFF
             },
             connect: function () {
                 this.$router.push('/login');
