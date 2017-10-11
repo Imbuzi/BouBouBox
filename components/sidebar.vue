5a86aa1f65d8467942471706c3aa2a8a9db1fd15
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-11 08:20:26 +0200

    Added frist sidebar content

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -1,6 +1,27 @@
 <template>
   <aside v-bind:class="{ open: opened }" class="right-sidebar">
     <div class="container-fluid">
+        <div class="row">
+            <ul class="nav nav-tabs tab-nav-right" role="tablist">
+                <li role="presentation" class="active"><a href="#settings" data-toggle="tab">PARAMÈTRES</a></li>
+                <li role="presentation"><a href="#widgets" data-toggle="tab">WIDGETS</a></li>
+            </ul>
+            <!-- Tab panes -->
+            <div class="tab-content">
+                <div role="tabpanel" class="tab-pane fade in active" id="settings">
+                    <b>Paramètres généraux</b>
+                </div>
+                <div role="tabpanel" class="tab-pane fade" id="widgets">
+                    <b>Widgets</b>
+                    <p>
+                        Lorem ipsum dolor sit amet, ut duo atqui exerci dicunt, ius impedit mediocritatem an. Pri ut tation electram moderatius.
+                        Per te suavitate democritum. Duis nemore probatus ne quo, ad liber essent aliquid
+                        pro. Et eos nusquam accumsan, vide mentitum fabellas ne est, eu munere gubergren
+                        sadipscing mel.
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
   </aside>
 </template>
