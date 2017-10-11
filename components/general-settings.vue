e1f35e9f0697946d83c8345fa989a1e77058b11c
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-11 09:00:33 +0200

    Improved general settings view

--- components/general-settings.vue
+++ components/general-settings.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="container-fluid">
         <div class="row">
-            <button v-bind:class="'bg-' + color" class="btn btn-default btn-circle">
-                <i class="material-icons">person</i>
-            </button>
-            <span class="badge">XX</span>
+            <div class="col-xs-12">
+                <button v-bind:class="'bg-' + color" class="btn btn-circle">
+                    <i class="material-icons">person</i>
+                </button>
+                <span class="badge notification-badge bg-black">1</span>
+            </div>
         </div>
     </div>
 </template>
@@ -18,3 +20,10 @@
         }
     }
 </script>
+
+<style scoped>
+    span.badge.notification-badge {
+        margin-left: -20px;
+        margin-top: -35px;
+    }
+</style>
