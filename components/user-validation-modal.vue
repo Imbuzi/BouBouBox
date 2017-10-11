521f450c375dcb500d78e73e3e43f19af08417c1
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-11 16:19:48 +0200

    Added user list parsing and display

--- components/user-validation-modal.vue
+++ components/user-validation-modal.vue
@@ -1,18 +1,25 @@
 <template>
     <div class="modal-content">
         <div class="modal-header">
-            <h4 class="modal-title">Modal title</h4>
+            <h4 class="modal-title">Utilisateurs en attente de validation</h4>
         </div>
         <div class="modal-body">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sodales orci ante, sed ornare eros vestibulum ut. Ut accumsan
-            vitae eros sit amet tristique. Nullam scelerisque nunc enim, non dignissim nibh faucibus ullamcorper.
-            Fusce pulvinar libero vel ligula iaculis ullamcorper. Integer dapibus, mi ac tempor varius, purus
-            nibh mattis erat, vitae porta nunc nisi non tellus. Vivamus mollis ante non massa egestas fringilla.
-            Vestibulum egestas consectetur nunc at ultricies. Morbi quis consectetur nunc.
+            <ul v-if="userList" class="list-group">
+                <li v-for="user in userList" class="list-group-item">{{user.name}} {{user.surname}}</li>
+            </ul>
         </div>
         <div class="modal-footer">
-            <button type="button" class="btn btn-link">SAVE CHANGES</button>
             <button type="button" class="btn btn-link" v-on:click.prevent="$emit('closeModal')">FERMER</button>
         </div>
     </div>
 </template>
+
+<script>
+    export default {
+        computed: {
+            userList: function () {
+                return this.$store.state.user.waitingForValidation;
+            }
+        }
+    }
+</script>
