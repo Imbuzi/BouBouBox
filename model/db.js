73bae908718ac11021fe119bbdf1719164fbb850
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-16 10:51:21 +0200

    Renamed knexfile to good conventional name

--- model/db.js
+++ model/db.js
@@ -1,5 +1,5 @@
 const environment = process.env.NODE_ENV || 'development';
-const config = require('../db-config.js')[environment];
+const config = require('../knexfile.js')[environment];
 const knex = require('knex')(config);
 
 const db = {
