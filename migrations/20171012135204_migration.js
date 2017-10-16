e78d9c431bed8556a03c0c03590e27b9851ade3b
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-16 10:27:11 +0200

    Edited knex migration

--- migrations/20171012135204_migration.js
+++ migrations/20171012135204_migration.js
@@ -1,8 +1,38 @@
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+    return Promise.all([
+        knex.schema.createTableIfNotExists('user', table => {
+            table.string('mail', 250).primary().notNullable()
+            table.string('name').notNullable()
+            table.string('surname', 100).notNullable()
+            table.string('password', 250).notNullable()
+            table.boolean('access').notNullable().defaultTo(false)
+        }),
+        knex.schema.createTableIfNotExists('widget', table => {
+            table.increments('id').primary().notNullable()
+            table.string('name', 100).notNullable()
+            table.enum('type', ['light'])
+        }),
+        knex.schema.createTableIfNotExists('bridge', table => {
+            table.increments('id').primary().notNullable()
+            table.string('mac', 17).notNullable()
+            table.string('name', 100).defaultTo(null)
+        })
+    ]).then(function () {
+        knex.schema.createTableIfNotExists('light', table => {
+            table.integer('router').notNullable().defaultTo(0)
+            table.foreign('router').references('bridge.id')
+            table.integer('zone').notNullable().defaulTo(0)
+            table.primary(['router', 'zone'])
+            table.enum('type', ['rgbw']).notNullable()
+            table.boolean('power').notNullable().defaultTo(false)
+            table.integer('intensity').notNullable().defaulTo(100)
+            table.integer('widget_id').notNullable()
+            table.foreign('widget_id').references('widget.id')
+        });
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTable('user')
 };
