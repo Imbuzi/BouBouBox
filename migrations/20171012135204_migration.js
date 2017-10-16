ec87dd208d58f2d1581654c04e2b075f0068a10b
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-16 11:17:26 +0200

    Corrected migration

--- migrations/20171012135204_migration.js
+++ migrations/20171012135204_migration.js
@@ -1,25 +1,37 @@
 exports.up = function (knex, Promise) {
-    return new Promise(function (resolve, reject) {
-        Promise.all([
-            knex.schema.createTableIfNotExists('user', table => {
+    return knex.raw('SET foreign_key_checks = 0;')
+        .then(function () {
+            return knex.schema.createTableIfNotExists('user', table => {
                 table.string('mail', 250).primary().notNullable()
                 table.string('name').notNullable()
                 table.string('surname', 100).notNullable()
                 table.string('password', 250).notNullable()
                 table.boolean('access').notNullable().defaultTo(false)
-            }),
-            knex.schema.createTableIfNotExists('widget', table => {
+            })
+        })
+        .then(function () {
+            return knex.schema.createTableIfNotExists('widget', table => {
                 table.increments('id').primary().notNullable()
                 table.string('name', 100).notNullable()
                 table.enum('type', ['light'])
-            }),
-            knex.schema.createTableIfNotExists('bridge', table => {
+            })
+        })
+        .then(function () {
+            return knex.schema.createTableIfNotExists('bridge', table => {
                 table.increments('id').primary().notNullable()
                 table.string('mac', 17).notNullable()
                 table.string('name', 100).defaultTo(null)
             })
-        ]).then(function () {
-            knex.schema.createTableIfNotExists('light', table => {
+        })
+        .then(function () {
+            return knex.schema.createTableIfNotExists('widget', table => {
+                table.increments('id').primary().notNullable()
+                table.string('name', 100).notNullable()
+                table.enum('type', ['light'])
+            })
+        })
+        .then(function () {
+            return knex.schema.createTableIfNotExists('light', table => {
                 table.integer('router').notNullable().defaultTo(0)
                 table.foreign('router').references('bridge.id')
                 table.integer('zone').notNullable().defaulTo(0)
@@ -30,19 +42,20 @@ exports.up = function (knex, Promise) {
                 table.integer('widget_id').notNullable()
                 table.foreign('widget_id').references('widget.id')
             })
-        }).then(function () {
-            resolve();
-        }).catch(function () {
-            reject();
         })
-    });
+        .finally(function () {
+            return knex.raw('SET foreign_key_checks = 1;');
+        });
 };
 
 exports.down = function (knex, Promise) {
-    return Promise.all([
-            knex.schema.dropTable('user'),
-            knex.schema.dropTable('widget'),
-            knex.schema.dropTable('bridge'),
-            knex.schema.dropTable('light')
-    ]);
+    return knex.raw('SET foreign_key_checks = 0;')
+        .then(function () {
+            return ['user', 'widget', 'bridge', 'light'];
+        })
+        .map(knex.schema.dropTable)
+        .finally(function () {
+            return knex.raw('SET foreign_key_checks = 1;');
+
+        });
 };
