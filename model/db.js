749f3b432a883ac8f281263769ea11b98c940a54
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-17 10:50:22 +0200

    Added bookshelf instance

--- model/db.js
+++ model/db.js
@@ -1,6 +1,7 @@
 const environment = process.env.NODE_ENV || 'development';
 const config = require('../knexfile.js')[environment];
 const knex = require('knex')(config);
+const bookshelf = require('bookshelf')(knex);
 
 const db = {
     widget: {},
