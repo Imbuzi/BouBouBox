e3a8ad740cf7a2581dcc69e57da9f5c00c98fe89
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-17 17:26:13 +0200

    Changed widget getter

--- model/db.js
+++ model/db.js
@@ -38,25 +38,16 @@ db.milight.setLightPower = function (light, value) {
 db.widget.getAll = function () {
     return knex
         .from('widget')
-        .leftOuterJoin('light', 'widget.id', 'light.widget_id')
-        .innerJoin('bridge', 'bridge.id', 'light.router')
-        .select('widget.id as id', 'widget.name as name', 'widget.type as type', 'light.type as light_type', 'light.zone as light_zone', 'light.intensity as light_intensity', 'light.power as light_power', 'bridge.mac as router_mac')
+        .select()
         .then(function (result) {
             result.forEach(function (element) {
-                element.light = {
-                    type: element.light_type,
-                    zone: element.light_zone,
-                    intensity: element.light_intensity,
-                    power: element.light_power,
-                    bridge: {
-                        mac: element.router_mac
-                    }
-                };
-                delete element.light_type;
-                delete element.light_zone;
-                delete element.light_intensity;
-                delete element.light_power;
-                delete element.router_mac;
+                knex
+                    .from('widget_' + element.widget_type)
+                    .select()
+                    .where('id', element.widget_type_id)
+                    .then(function (result) {
+                        element[element.widget_type] = result.first();
+                    })
             });
 
             return result;
