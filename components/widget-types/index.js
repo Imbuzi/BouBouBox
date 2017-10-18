75778738ac0541996adee24d40c1396c74eaecd0
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-18 15:49:21 +0200

    Updated getDirectories

--- components/widget-types/index.js
+++ components/widget-types/index.js
@@ -1,4 +1,8 @@
-console.log(FS);
+const fs = require('fs');
+const path = require('path');
+
+console.log(fs);
+console.log(path);
 
 const getDirectories = srcPath => fs.readdirSync(srcPath).filter(file => fs.statSync(path.join(srcPath, file)).isDirectory())
 
