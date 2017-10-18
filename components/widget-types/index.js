faeeec8eb8cfbe5f2c0985d415cf81b1b78fe377
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-18 15:41:56 +0200

    Added logging

--- components/widget-types/index.js
+++ components/widget-types/index.js
@@ -1,6 +1,8 @@
 import fs from 'fs'
 import path from 'path'
 
+console.log(fs);
+
 const getDirectories = srcPath => fs.readdirSync(srcPath).filter(file => fs.statSync(path.join(srcPath, file)).isDirectory())
 
 console.log(getDirectories('./'));
