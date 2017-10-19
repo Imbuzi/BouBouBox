1ff4a5e72f29d0ba74ccd85177a000bd34d8c339
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-19 16:01:57 +0200

    Corrected wrong call

--- socket/index.js
+++ socket/index.js
@@ -1,6 +1,3 @@
-// TODO : Remove API
-const api = require('../api');
-
 module.exports = function (io) {
     let module = {};
 
@@ -11,7 +8,8 @@ module.exports = function (io) {
                 console.log('[SOCKET.IO] User connected with ID ' + socket.id);
             }
 
-            require("fs").readdirSync(require("path").join(__dirname, "request")).forEach(function (file) {
+            let dir = require("path").join(__dirname, "request")
+            require("fs").readdirSync(dir).forEach(function (file) {
                 let name = file.replace(/\.[^/.]+$/, "");
                 requests[name] = require("./request/" + file)(socket, io);
             });
