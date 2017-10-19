20490a37e307caf964c2bdba666c22a202074d14
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-19 15:06:02 +0200

    Created entry point for socket file and added main listener

---
+++ socket/index.js
@@ -0,0 +1,112 @@
+module.exports = function (app, io) {
+    let module = {};
+
+    module.listen = function() {
+        io.on('connection', function (socket) {
+            socket.on('getWidgetList', function (token) {
+                api.validateToken(token).then(function () {
+                    api.getWidgetList().then(function (widgetList) {
+                        socket.emit('widgetList', widgetList);
+                    }).catch(function (error) {
+                        socket.emit('widgetList', error);
+                    });
+                }).catch(function (result) {
+                    socket.emit('widgetList', result);
+                });
+            });
+
+            socket.on('refuseNewUser', function (token, mail) {
+                api.validateToken(token).then(function () {
+                    api.refuseNewUser(mail).then(function (result) {
+                        socket.emit('userRefused', result);
+                        socket.broadcast.emit('userRefused', result);
+                    }).catch(function (result) {
+                        socket.emit('userRefused', result);
+                    });
+                }).catch(function (result) {
+                    socket.emit('userRefused', result);
+                });
+            });
+
+            socket.on('acceptNewUser', function (token, mail) {
+                api.validateToken(token).then(function () {
+                    api.acceptNewUser(mail).then(function (result) {
+                        socket.emit('userAccepted', result);
+                        socket.broadcast.emit('userAccepted', result);
+                    }).catch(function (result) {
+                        socket.emit('userAccepted', result);
+                    });
+                }).catch(function (result) {
+                    socket.emit('userRefused', result);
+                });
+            });
+
+            socket.on('getUsersWaitingForValidation', function (token) {
+                api.validateToken(token).then(function () {
+                    api.getUsersWaitingForValidation().then(function (result) {
+                        socket.emit('usersWaitingForValidation', result);
+                    }).catch(function (result) {
+                        socket.emit('usersWaitingForValidation', result);
+                    });
+                }).catch(function (result) {
+                    socket.emit('usersWaitingForValidation', result);
+                });
+            });
+
+            socket.on('getJWT', function (data) {
+                api.getJWT(data.mail, data.password).then(function (result) {
+                    socket.emit('JWT', result);
+                }).catch(function (result) {
+                    socket.emit('JWT', result);
+                });
+            });
+
+            socket.on('addUser', function (data) {
+                api.addUser(data.name, data.surname, data.mail, data.password).then(function (result) {
+                    socket.emit('userAdded', result);
+                    socket.broadcast.emit('userAdded', result);
+                }).catch(function (result) {
+                    socket.emit('userAdded', result);
+                });
+            });
+
+            socket.on('setLightIntensity', function (token, data) {
+                api.validateToken(token).then(function () {
+                    api.milight.setLightIntensity(data.value, data.light).then(function (result) {
+                        socket.broadcast.emit('lightIntensity', result);
+                    }).catch(function (error) {
+                        console.log(error);
+                    });
+                }).catch(function (result) {
+                    console.log(result);
+                });
+            });
+
+            socket.on('setLightColor', function (token, data) {
+                api.validateToken(token).then(function () {
+                    api.milight.setLightColor(data.value, data.light).then(function (result) {
+                        console.log(result);
+                    }).catch(function (error) {
+                        console.log(error);
+                    });
+                }).catch(function (result) {
+                    console.log(result);
+                });
+            });
+
+            socket.on('setLightPower', function (token, data) {
+                api.validateToken(token).then(function () {
+                    api.milight.setLightPower(data.value, data.light).then(function (result) {
+                        socket.broadcast.emit('lightPower', result);
+                    }).catch(function (error) {
+                        console.log(error);
+                    });
+                }).catch(function (result) {
+                    console.log(result);
+                });
+            });
+        });
+    }
+
+    return module;
+};
