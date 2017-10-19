baf2ff8fe673a271996721c45977674edff2d80e
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-19 15:23:20 +0200

    Corrected IO reference

--- socket/index.js
+++ socket/index.js
@@ -10,7 +10,7 @@ const api = require('../api');
 
 module.exports = function (app, io) {
     let module = {};
-    console.log(io);
+    let io = io;
 
     module.listen = function() {
         io.on('connection', function (socket) {
