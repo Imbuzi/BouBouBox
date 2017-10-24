f9f1c89b3dc8e53a52aac3bb1110cdb3fdeaacf9
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-24 14:32:27 +0200

    Corrected wrong function name

--- api/db/index.js
+++ api/db/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 let db = {};
 fs.readdirSync(__dirname).map(function (f) {
     let stat = fs.statSync(path.join(__dirname, f));
-    if (stat.isFile()) {
+    if (stat.isDirectory()) {
         db[f] = require(path.join(__dirname, f));
     }
 });
