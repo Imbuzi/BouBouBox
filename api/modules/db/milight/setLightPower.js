3b62119409dcc0af0edfbe0080e0eda369c21b83
Imbuzi <nicolas.bourasseau@outlook.com>
2017-10-27 17:41:12 +0200

    Removed unused logging

--- api/modules/db/milight/setLightPower.js
+++ api/modules/db/milight/setLightPower.js
@@ -3,8 +3,6 @@ const config = require(require('path').dirname(require.main.filename) + '/knexfi
 const knex = require('knex')(config);
 
 module.exports = function (light, value) {
-    console.log(light);
-    console.log(value);
     return knex
         .from('widget_milight')
         .where({
